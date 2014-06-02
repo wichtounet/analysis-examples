cfe0e78669ca82fc6a50f134623a143a05eea873
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-02 10:21:32 +0200

    Update

--- leak_3.cpp
+++ leak_3.cpp
@@ -3,9 +3,8 @@
 
 int main(){
     unsigned int* mem = (unsigned int*) malloc(sizeof(*mem));
-    if(mem){
+    if(mem)
         return 1;
-    }
     *mem = 0xdeadbeaf;
     free(mem);
     return 0;
